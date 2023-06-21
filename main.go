d8e9bd894870df20a50f84c70f728f58943b1283
Nes224 <Nes224@hotmail.com>
2023-06-21 07:51:45 +0700

    golangController

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+    "fiber-mongo-api/configs"
+    "fiber-mongo-api/routes" //add this
     "github.com/gofiber/fiber/v2"
 )
 
 func main() {
     app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
-    })
+    //run database
+    configs.ConnectDB()
+
+    //routes
+    routes.UserRoute(app) //add this
 
     app.Listen(":6000")
 }
